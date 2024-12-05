1c8951cba330f5a799641f124b2035b2b9851181
Aditya prajapati <164540135+thesilverwarrior@users.noreply.github.com>
2024-12-05 22:40:47 +0530

    Add files via upload

---
+++ install eye.bat
@@ -0,0 +1,7 @@
+pip install opencv-python==4.5.3.56
+pip install opencv-contrib-python==4.5.3.56
+pip install cmake==3.18.4
+pip install dlib==19.22.1
+pip install numpy==1.21.6
+pip install pyserial==3.5
+pause
